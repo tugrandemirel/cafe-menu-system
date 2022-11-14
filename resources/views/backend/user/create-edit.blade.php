6fdb8a811524bd64b0a75ee3df27e2c2bb143297
Tugran Demirel <demireltugran66@gmail.com>
2022-11-14 22:34:13 +0300

    Kullanıcı oluşturma sayfasının oluşturulması

---
+++ resources/views/backend/user/create-edit.blade.php
@@ -0,0 +1,45 @@
+@extends('backend.layouts.app')
+@section('site_name', 'Kullanıcı Ekle')
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+@endsection
+@section('content')
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+            <div class="col-sm-12 col-xl-12">
+                <div class="bg-light rounded h-100 p-4">
+                    @if($errors->any() && \Session::has('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <i class="fa fa-exclamation-circle me-2"></i>{{$errors->first()}}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+
+                    @if($errors->any() && \Session::has('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <i class="fa fa-exclamation-circle me-2"></i>{{$errors->first()}}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="col-sm-12 col-xl-12">
+                <div class="bg-light rounded h-100 p-4">
+                    <h6 class="mb-4">Kullanıcı Ekle</h6>
+                    <form action="{{ route('admin.product.store') }}" method="POST"  enctype="multipart/form-data">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="name" class="col-sm-2 col-form-label">Ad Soyad</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kaydet</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+@endsection
